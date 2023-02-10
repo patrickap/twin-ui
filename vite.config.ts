ce83a830643566a783858a004b4f9f904fa15a1d
patrickap <patrick.schlageter@web.de>
2023-02-10 22:29:48 +0100

    fix: routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
   server: {
     port: 3000,
     host: true,
