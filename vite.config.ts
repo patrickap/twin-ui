7a376d39d2a459813c9bd90c1a0a6db373383a9c
patrickap <patrick.schlageter@web.de>
2023-02-11 00:17:12 +0100

    fix: use index html inside src root

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
   server: {
     port: 3000,
     host: true,
