7ac359c1b3245776a032714c251aa7f018c20fd9
patrickap <patrick.schlageter@web.de>
2023-02-13 18:46:01 +0100

    feat: logo text

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/layout';
+import { Flex, Text } from '@chakra-ui/layout';
 import { Icon, useColorModeValue } from '@chakra-ui/react';
 import { BoltIcon } from '@heroicons/react/20/solid';
 
@@ -6,8 +6,11 @@ export const Logo = () => {
   const color = useColorModeValue('brand.500', 'brand.200');
 
   return (
-    <Flex justifyContent='center'>
+    <Flex justifyContent='center' alignItems='center'>
       <Icon as={BoltIcon} w={50} h={50} color={color} />
+      <Text>
+        <b>sftp</b>go
+      </Text>
     </Flex>
   );
 };
