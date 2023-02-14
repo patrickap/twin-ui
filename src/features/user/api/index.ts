6f24734abb9d9ddc348679f74fad35ed9d3f6d7c
patrickap <patrick.schlageter@web.de>
2023-02-14 21:11:50 +0100

    feat: installed urlcat utility

--- src/features/user/api/index.ts
+++ src/features/user/api/index.ts
@@ -2,6 +2,7 @@ import { User } from '../types';
 
 // TODO: implement user api
 const getUser = (id?: string) => {
+  // const url = urlcat(import.meta.env.VITE_API_BASE_URL, '/user');
   // TODO: use ky.post(...)
   return Promise.resolve({
     id: '1',
