35f6bbab08dd214397021820a2002911eb84276b
patrickap <patrick.schlageter@web.de>
2023-02-15 10:46:42 +0100

    feat: configured import aliases + only allow features import via root index

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   root: 'src',
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   server: {
     port: 3000,
     host: true,
