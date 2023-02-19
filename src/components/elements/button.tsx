ebadf7c6d98659aba4c33ed9a8ad3010a4e8648e
patrickap <patrick.schlageter@web.de>
2023-02-19 23:55:28 +0100

    fix: whitespace

--- src/components/elements/button.tsx
+++ src/components/elements/button.tsx
@@ -22,7 +22,7 @@ const Button = ({
       className={clsx(
         'inline-flex w-full justify-center rounded-md border px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto sm:text-sm',
         color === 'default' &&
-          'border-slate-300 bg-white  text-slate-700 hover:bg-slate-50 focus:ring-brand-500',
+          'border-slate-300 bg-white text-slate-700 hover:bg-slate-50 focus:ring-brand-500',
         color === 'brand' &&
           'border-transparent bg-brand-500 text-white hover:bg-brand-600 focus:ring-brand-400',
         color === 'info' &&
