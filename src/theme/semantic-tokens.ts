f37abd46fa1efb1d42dc155c5ce867d4b0ce6aa3
patrickap <patrick.schlageter@web.de>
2023-02-19 12:09:15 +0100

    feat: semantic tokens for text

--- src/theme/semantic-tokens.ts
+++ src/theme/semantic-tokens.ts
@@ -1,3 +1,22 @@
-const semanticTokens = {};
+const semanticTokens = {
+  colors: {
+    'text-default': {
+      default: 'gray.700',
+      _dark: 'gray.300',
+    },
+    'text-error': {
+      default: 'red.500',
+      _dark: 'red.400',
+    },
+    'text-success': {
+      default: 'green.500',
+      _dark: 'green.300',
+    },
+    'text-info': {
+      default: 'blue.600',
+      _dark: 'blue.300',
+    },
+  },
+};
 
 export { semanticTokens };
