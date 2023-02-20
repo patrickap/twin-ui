09c6238357533a6d6baf5bd794f7592cb30b9810
patrickap <patrick.schlageter@web.de>
2023-02-20 23:33:24 +0100

    fix: consistent focus ring

--- src/components/elements/button.tsx
+++ src/components/elements/button.tsx
@@ -24,17 +24,17 @@ const Button = ({
       className={clsx(
         'inline-flex w-full justify-center rounded-md border px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto sm:text-sm',
         color === 'default' &&
-          'border-slate-300 bg-white text-slate-700 hover:bg-slate-50 focus:ring-brand-400',
+          'border-slate-300 bg-white text-slate-700 hover:bg-slate-50 focus:ring-brand-500',
         color === 'brand' &&
-          'border-transparent bg-brand-500 text-white hover:bg-brand-600 focus:ring-brand-400',
+          'border-transparent bg-brand-500 text-white hover:bg-brand-600 focus:ring-brand-500',
         color === 'info' &&
-          'border-transparent bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-400',
+          'border-transparent bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-500',
         color === 'warning' &&
-          'border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:ring-yellow-400',
+          'border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:ring-yellow-500',
         color === 'error' &&
-          'border-transparent bg-red-500 text-white hover:bg-red-600 focus:ring-red-400',
+          'border-transparent bg-red-500 text-white hover:bg-red-600 focus:ring-red-500',
         color === 'success' &&
-          'border-transparent bg-green-500 text-white hover:bg-green-600 focus:ring-green-400',
+          'border-transparent bg-green-500 text-white hover:bg-green-600 focus:ring-green-500',
         isDisabled &&
           '!hover:bg-slate-100 !cursor-not-allowed !border-slate-200 !bg-slate-100 !text-slate-300',
         isLoading && !isDisabled && '!cursor-not-allowed',
--- src/components/elements/checkbox.tsx
+++ src/components/elements/checkbox.tsx
@@ -43,7 +43,7 @@ const Checkbox = forwardRef(
           onChange={onChange}
           onBlur={onBlur}
           className={clsx(
-            'h-4 w-4 cursor-pointer rounded border border-slate-300 text-brand-500 focus:ring-brand-400',
+            'h-4 w-4 cursor-pointer rounded border border-slate-300 text-brand-500 focus:ring-2 focus:ring-brand-500 focus:ring-offset-2',
             isDisabled && '!cursor-not-allowed !border-slate-200 !bg-slate-100',
             isError && !isDisabled && '!border-red-500',
           )}
