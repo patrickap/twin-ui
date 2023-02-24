8612250ce13e4de611ca2cf3916356518490cdb1
patrickap <patrick.schlageter@web.de>
2023-02-24 21:29:16 +0100

    fix: switched to popover

--- src/components/elements/drawer.tsx
+++ src/components/elements/drawer.tsx
@@ -1,4 +1,4 @@
-import { Dialog, Transition } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { Fragment, ReactNode } from 'react';
 import { ButtonIcon } from './button-icon';
@@ -12,11 +12,7 @@ type DrawerProps = {
 const Drawer = ({ isOpen = false, onClose, children }: DrawerProps) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog
-        as='div'
-        onClose={() => onClose?.(false)}
-        className='relative z-10'
-      >
+      <Popover as='div' className='relative z-10'>
         <Transition.Child
           as={Fragment}
           enter='ease-out duration-300'
@@ -26,7 +22,10 @@ const Drawer = ({ isOpen = false, onClose, children }: DrawerProps) => {
           leaveFrom='opacity-100'
           leaveTo='opacity-0'
         >
-          <Dialog.Overlay className='fixed inset-0 bg-slate-500/25' />
+          <Popover.Overlay
+            className='fixed inset-0 bg-slate-500/25'
+            onClick={() => onClose?.(false)}
+          />
         </Transition.Child>
 
         <Transition.Child
@@ -39,17 +38,17 @@ const Drawer = ({ isOpen = false, onClose, children }: DrawerProps) => {
           leaveTo='-translate-x-full'
         >
           <div className='fixed left-0 top-0 h-screen w-full max-w-xs'>
-            <Dialog.Panel className='flex h-full flex-col overflow-hidden bg-white shadow-lg'>
+            <Popover.Panel className='flex h-full flex-col overflow-hidden bg-white shadow-lg'>
               <div className='flex justify-end p-4'>
                 <ButtonIcon onClick={() => onClose?.(false)}>
                   <XMarkIcon className='h-5 w-5' />
                 </ButtonIcon>
               </div>
               <div>{children}</div>
-            </Dialog.Panel>
+            </Popover.Panel>
           </div>
         </Transition.Child>
-      </Dialog>
+      </Popover>
     </Transition>
   );
 };
