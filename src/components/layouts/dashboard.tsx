ab22ddcff37d57dfab7bfd788ca4737fe90791e7
patrickap <patrick.schlageter@web.de>
2023-02-24 22:49:25 +0100

    feat: nav click closes drawer

--- src/components/layouts/dashboard.tsx
+++ src/components/layouts/dashboard.tsx
@@ -6,6 +6,36 @@ type DashboardProps = {
   children?: ReactNode;
 };
 
+const Dashboard = ({ children }: DashboardProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className='flex h-screen w-full'>
+      <aside className='hidden w-20 border border-r border-slate-200 bg-white md:block lg:w-64'>
+        <div className='px-4 py-8'>
+          <Navigation />
+        </div>
+      </aside>
+      <main className='flex flex-grow flex-col gap-1 p-4 md:p-8'>
+        <div className='flex items-center justify-between'>
+          <span className='text-sm text-slate-500'>bread / crumbs / here</span>
+          <div className='md:hidden'>
+            <ButtonIcon onClick={() => setIsOpen(true)}>
+              <Bars2Icon className='h-5 w-5' />
+            </ButtonIcon>
+            <Drawer isOpen={isOpen} onClose={() => setIsOpen(false)}>
+              <div className='p-4 pt-0' onClick={() => setIsOpen(false)}>
+                <Navigation />
+              </div>
+            </Drawer>
+          </div>
+        </div>
+        <div>{children}</div>
+      </main>
+    </div>
+  );
+};
+
 const Navigation = () => {
   const items = [
     {
@@ -35,36 +65,6 @@ const Navigation = () => {
         </div>
       ))}
     </Nav>
-  );
-};
-
-const Dashboard = ({ children }: DashboardProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div className='flex h-screen w-full'>
-      <aside className='hidden w-20 border border-r border-slate-200 bg-white md:block lg:w-64'>
-        <div className='px-4 py-8'>
-          <Navigation />
-        </div>
-      </aside>
-      <main className='flex flex-grow flex-col gap-1 p-4 md:p-8'>
-        <div className='flex items-center justify-between'>
-          <span className='text-sm text-slate-500'>bread / crumbs / here</span>
-          <div className='md:hidden'>
-            <ButtonIcon onClick={() => setIsOpen(true)}>
-              <Bars2Icon className='h-5 w-5' />
-            </ButtonIcon>
-            <Drawer isOpen={isOpen} onClose={() => setIsOpen(false)}>
-              <div className='p-4 pt-0'>
-                <Navigation />
-              </div>
-            </Drawer>
-          </div>
-        </div>
-        <div>{children}</div>
-      </main>
-    </div>
   );
 };
 
