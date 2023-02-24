fb88091b6017d22bdb1da55a199e6af80fba40a3
patrickap <patrick.schlageter@web.de>
2023-02-25 00:17:39 +0100

    chore: added todo

--- src/components/elements/dropdown-item.tsx
+++ src/components/elements/dropdown-item.tsx
@@ -12,6 +12,7 @@ const DropdownItem = ({ icon, onClick, children }: DropdownItemProps) => {
       onClick={onClick}
       className='flex w-full items-center gap-2 rounded-lg bg-white p-2 text-sm text-slate-700 hover:bg-brand-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2'
     >
+      {/* TODO: FIXME: hover icon color */}
       {icon ? <span className='h-5 w-5 text-slate-700'>{icon}</span> : null}
       {children ? <span>{children}</span> : null}
     </button>
