ff7cedab9ca9d22885696e186bfc013153697a76
patrickap <patrick.schlageter@web.de>
2023-03-02 21:58:03 +0100

    feat: improved drawer

--- src/components/elements/drawer.tsx
+++ src/components/elements/drawer.tsx
@@ -1,6 +1,7 @@
 import { Bars2Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import * as dialog from '@radix-ui/react-dialog';
-import { ReactNode } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ReactNode, useState } from 'react';
 import { ButtonIcon } from './button-icon';
 
 type DrawerProps = {
@@ -8,24 +9,42 @@ type DrawerProps = {
 };
 
 const Drawer = ({ children }: DrawerProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <dialog.Root>
+    <dialog.Root open={isOpen} onOpenChange={setIsOpen}>
       <dialog.Trigger>
         <ButtonIcon>
           <Bars2Icon className='h-5 w-5' />
         </ButtonIcon>
       </dialog.Trigger>
-      <dialog.Portal>
-        <dialog.Overlay className='fixed inset-0 z-10 bg-slate-500/25' />
-        <dialog.Content className='fixed top-0 left-0 z-10 flex h-full w-full max-w-xs flex-col overflow-hidden bg-white shadow-lg'>
-          <dialog.Close className='flex justify-end p-4'>
-            <ButtonIcon>
-              <XMarkIcon className='h-5 w-5' />
-            </ButtonIcon>
-          </dialog.Close>
-          <div>{children}</div>
-        </dialog.Content>
-      </dialog.Portal>
+      <AnimatePresence>
+        {isOpen ? (
+          <dialog.Portal forceMount>
+            <dialog.Overlay className='fixed inset-0 z-10 bg-slate-500/25' />
+            <dialog.Content
+              asChild
+              className='fixed top-0 left-0 z-10 flex h-full w-full max-w-xs flex-col overflow-hidden bg-white shadow-lg'
+            >
+              <motion.div
+                initial={{ translateX: '-100%' }}
+                animate={{ translateX: 0 }}
+                exit={{ translateX: '-100%' }}
+                transition={{
+                  duration: 0.2,
+                }}
+              >
+                <dialog.Close className='flex justify-end p-4'>
+                  <ButtonIcon>
+                    <XMarkIcon className='h-5 w-5' />
+                  </ButtonIcon>
+                </dialog.Close>
+                <div>{children}</div>
+              </motion.div>
+            </dialog.Content>
+          </dialog.Portal>
+        ) : null}
+      </AnimatePresence>
     </dialog.Root>
   );
 };
