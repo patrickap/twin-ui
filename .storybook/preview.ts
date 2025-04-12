799544dd25bcdc776df7a32063e7ad990a5f6d6d
patrickap <patrick.schlageter@web.de>
2025-04-12 22:56:03 +0200

    chore: comment

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,7 @@
 import type { Preview } from "@storybook/react";
+// This import generates a warning in the console:
+// Files in the public directory are served at the root path.
+// Which can be ignored as it's not an issue in preview mode.
 import "../public/index.css";
 
 const preview: Preview = {
