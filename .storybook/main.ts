f409c0010ae36b3356cc1ca79d21d446125efaaf
patrick <patrick@noreply.local>
2025-11-20 00:03:36 +0100

    fix: missing plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
-  ],
+  addons: [],
   framework: {
     name: "@storybook/react-vite",
     options: {},
